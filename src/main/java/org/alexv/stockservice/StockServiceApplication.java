56cfe44f0ce64e03b64470602ea299dc6cd570e2
alex <varaticalexandru1@gmail.com>
2024-03-19 17:15:26 +0200

    upd app-bootstrapping-class: - add loadEnvVars() static method for .env injection; - invoke .env injection before any Spring context laoding;

--- src/main/java/org/alexv/stockservice/StockServiceApplication.java
+++ src/main/java/org/alexv/stockservice/StockServiceApplication.java
@@ -1,5 +1,6 @@
 package org.alexv.stockservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,7 +12,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class StockServiceApplication {
 
     public static void main(String[] args) {
+        loadEnvVars();
         SpringApplication.run(StockServiceApplication.class, args);
+    }
+
+    public static void loadEnvVars() {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
     }
 
 }
